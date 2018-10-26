ddce5030ac08d05f339c72ebc27f3b52ca864f98
mummyy <42933101+mummyy@users.noreply.github.com>
2018-10-26 11:11:28 +0530

    Add files via upload

---
+++ labs61.py
@@ -0,0 +1,14 @@
+import os
+from time import sleep
+
+def mymail():
+	return
+
+def takesnap():
+	os.system("fswebcam -F 4 /home/dl114/Rahulroy/img/tmp.jpg")
+	return
+
+for i in range(10):
+	takesnap()
+	mymail()
+	sleep(5)
